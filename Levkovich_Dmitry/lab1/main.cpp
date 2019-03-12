986dca00dc5ecc39b37c54b8d7d83a673c341901
DmLvkvch <31544742+DmLvkvch@users.noreply.github.com>
2019-03-12 14:13:07 +0300

    Add files via upload

--- Levkovich_Dmitry/lab1/main.cpp
+++ Levkovich_Dmitry/lab1/main.cpp
@@ -12,52 +12,19 @@ public:
         size = N;
         bestcolor = 16;
         color = 4;
-        count = 3;
+        count = 1;
     }
     ~Square(){
 
     }
+    void write(){
+        for(int i = 0;i<size;i++){
+            for(int j = 0;j<size;j++)
+                cout<<best[i][j]<<" ";
+            cout<<endl;
+        }
 
-    void solve(){
-        if(size%2 == 0){
-            PutSquare(0, 0, size/2, 1, size/2);
-            PutSquare(0, size/2,size/2, 2,size/2);
-            PutSquare(size/2, 0,size/2, 3,size/2);
-            BackTracking( size/2, size/2, size-size/2);
-            ans();
-        }
-        else if(size%3 == 0){
-            PutSquare(0, 0, 2*size/3, 1,size/3);
-            PutSquare(0, 2*size/3, size/3, 2,size/3);
-            PutSquare(2*size/3, 0, size/3, 3,size/3);
-            BackTracking( 2*size/3, size/3, size/3);
-            ans();
-        }
-        else if(size%5 == 0){
-            PutSquare(0, 0, 3*size/5, 1,2*size/5);
-            PutSquare(0, 3*size/5, 2*size/5, 2,2*size/5);
-            PutSquare(3*size/5, 0, 2*size/5, 3,2*size/5);
-            BackTracking(3*size/5, 2*size/5, 2*size/5);
-            ans();
-        }
-        else{
-            PutSquare(0,0,size/2+1,1,size-(size/2+1));
-            PutSquare(size/2+1,0,size/2,2,size-(size/2+1));
-            PutSquare(0,size/2+1,size/2,3,size-(size/2+1));
-            BackTracking(size/2+1, size/2, size-(size/2+1));
-            ans();
-        }
     }
-
-private:
-    vector<vector<int>> best;
-    vector<vector<int>> arr;
-    int size;
-    int bestcolor;
-    int color;
-    int count;
-    vector<vector<int>> q;
-
     void RemoveSqr(int N, int color){
         for(auto i = N; i<size;i++)
             for(auto j = N; j < size; j++){
@@ -68,8 +35,8 @@ private:
     }
 
     bool findEmptySquare(int & x, int & y, int N){
-        for (y = N;y<size;y++) {
-            for (x = N;x<size;x++) {
+        for (x = N;x<size;x++) {
+            for (y = N;y<size;y++) {
                 if(arr[y][x]==0)
                     return true;
             }
@@ -99,27 +66,46 @@ private:
                 }
             }
         }
-
         return true;
     }
 
-    void BackTracking(int x, int y, unsigned N){
-        if(color-1>bestcolor){
+
+    void BackTracking(int x, int y, int N){
+
+        if(color>bestcolor){
+
             return;
         }
-        if(findEmptySquare(x, y, N)){
-            for(int i = N; i > 0;i--){
-                if(PutSquare(x, y, i, color, N)){
-                    color++;
-                    BackTracking(x, y, N);
-                    color--;
+        for(int i = N ; i >= 1; i--)
+        {
+            if(PutSquare(x, y, i, color, N))
+            {
+
+                color++;
+
+                int x_ = x;
+                int y_ = y;
+                if(findEmptySquare(x_, y_, N)){
+
+                    BackTracking(x_, y_, N);
+
                 }
-             RemoveSqr(N, color);
+                else
+
+                {
+
+                    color--;
+                    if(color < bestcolor){
+
+                        bestcolor = color;
+                        print(bestcolor);
+                    }
+                    RemoveSqr(N, color);
+                    return;
+                }
+                color--;
+                RemoveSqr(N, color);
             }
-        }
-        else if((color-1) < bestcolor){
-            bestcolor = color-1;
-            print(bestcolor);
         }
     }
 
@@ -157,12 +143,63 @@ private:
             best[k-1][2] = size;
         }
     }
-    void ans(){
-        cout << bestcolor << endl;
-        for(int i = 0;i<bestcolor;i++){
-           cout<<best[i][0]<<" "<<best[i][1]<<" "<<best[i][2]<<endl;
+
+    void solve(){
+
+        if(size%2 == 0){
+            PutSquare(0, 0, size/2, 1, size/2);
+            PutSquare(0, size/2,size/2, 2,size/2);
+            PutSquare(size/2, 0,size/2, 3,size/2);
+            BackTracking( size/2, size/2, size-size/2);
+            cout << bestcolor << endl;
+            for(int i = 0;i<bestcolor;i++){
+                cout<<best[i][0]<<" "<<best[i][1]<<" "<<best[i][2]<<endl;
+            }
+        }
+        else if(size%3 == 0){
+            PutSquare(0, 0, 2*size/3, 1,size/3);
+            PutSquare(0, 2*size/3, size/3, 2,size/3);
+            PutSquare(2*size/3, 0, size/3, 3,size/3);
+            BackTracking( 2*size/3, size/3, size/3);
+            cout << bestcolor << endl;
+            for(int i = 0;i<bestcolor;i++){
+                cout<<best[i][0]<<" "<<best[i][1]<<" "<<best[i][2]<<endl;
+            }
+        }
+        else if(size%5 == 0){
+
+            PutSquare(0, 0, 3*size/5, 1,2*size/5);
+                        PutSquare(0, 3*size/5, 2*size/5, 2,2*size/5);
+                        PutSquare(3*size/5, 0, 2*size/5, 3,2*size/5);
+
+            BackTracking(3*size/5, 2*size/5, 2*size/5);
+            cout << bestcolor << endl;
+            for(int i = 0;i<bestcolor;i++){
+                cout<<best[i][0]<<" "<<best[i][1]<<" "<<best[i][2]<<endl;
+            }
+        }
+
+        else{
+            color++;
+            PutSquare(0,0,size/2+1,1,size-(size/2+1));
+            PutSquare(size/2+1,0,size/2,2,size-(size/2+1));
+            PutSquare(0,size/2+1,size/2,3,size-(size/2+1));
+            BackTracking(size/2+1, size/2, size-(size/2+1));
+            cout << bestcolor << endl;
+            for(int i = 0;i<bestcolor;i++){
+                cout<<best[i][0]<<" "<<best[i][1]<<" "<<best[i][2]<<endl;
+            }
+
         }
     }
+
+private:
+    vector<vector<int>> best;
+    vector<vector<int>> arr;
+    int size;
+    int bestcolor;
+    int color;
+    int count;
 };
 
 int main()
